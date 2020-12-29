239484d3f8ea53d1417aeed23d20dfebf81d1043
dnsrocha <drocha@gmail.com>
2020-12-28 23:38:16 -0800

    Wave 1

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Timeline.css';
 import TimelineEvent from './TimelineEvent';
 
-const Timeline = () => {
+const Timeline = (props) => {
   
   return;
 }
--- src/components/TimelineEvent.js
+++ src/components/TimelineEvent.js
@@ -2,9 +2,16 @@ import React from 'react';
 import './TimelineEvent.css';
 import Timestamp from './Timestamp';
 
-const TimelineEvent = () => {
+const TimelineEvent = (props) => {
   
-  return;
+  return (
+    <div className="timeline-event">
+      <div className="event-person"> { props.person } </div>
+      <div className="event-status"> { props.status } </div>
+      <div className="event-timestamp"> <Timestamp time={props.timestamp}/></div>
+    </div>
+  );
+
 }
 
 export default TimelineEvent;
